c87490c52c75cfe31dd30531ea09a8f47ffa4e45
Julia Andreeva <andreeva.js.direct@gmail.com>
2022-05-17 13:50:25 +0500

    History Item

--- app/src/main/java/com/example/ubc/ui/panel/items/ubcarduino/HistoryView.kt
+++ app/src/main/java/com/example/ubc/ui/panel/items/ubcarduino/HistoryView.kt
@@ -3,10 +3,13 @@ package com.example.ubc.ui.panel.items.ubcarduino
 import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context
-import android.content.Intent
 import android.util.AttributeSet
 import android.view.LayoutInflater
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.PopupMenu
 import androidx.core.content.ContextCompat
+import com.example.ubc.R
 import com.example.ubc.databinding.ItemHistoryBinding
 import com.example.ubc.items.smf.ItemHistory
 import com.example.ubc.ui.panel.items.ItemView
@@ -21,16 +24,14 @@ class HistoryView @JvmOverloads constructor(
     private val binding = ItemHistoryBinding.inflate(LayoutInflater.from(context),this,true)
 
     init{
-        binding.historyCopyButton.setOnClickListener { copy() }
-        binding.historyClearButton.setOnClickListener {
-            item.history.clear()
-            binding.historyDisplay.text = ""
+        binding.itemHistoryOptionsBtn.setOnClickListener {
+            showOptions()
         }
     }
 
     override fun onDataReceived(data: ByteArray) {
         item.onDataReceived(data)
-        binding.historyDisplay.text = item.history.joinToString("\r\n")
+        binding.itemHistoryText.text = item.history.joinToString("\r\n")
     }
 
     private fun copy() {
@@ -41,11 +42,30 @@ class HistoryView @JvmOverloads constructor(
         clipboardManager?.setPrimaryClip(clipData)
     }
 
-    private fun share() {
-        val intent= Intent()
-        intent.action= Intent.ACTION_SEND
-        intent.putExtra(Intent.EXTRA_TITLE, "Connection logs")
-        intent.type="text/plain"
-        context.startActivity(Intent.createChooser(intent, "Share To:"))
+    private fun clear() {
+        binding.itemHistoryText.clearComposingText()
+        item.history.clear()
+    }
+
+    private fun showOptions() {
+        val popup = PopupMenu(context, binding.itemHistoryOptionsBtn)
+        val inflater: MenuInflater = popup.menuInflater
+        inflater.inflate(R.menu.item_history_options_menu, popup.menu)
+        popup.setOnMenuItemClickListener{item -> onMenuOptionClick(item)}
+        popup.show()
+    }
+
+    private fun onMenuOptionClick(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.item_history_options_copy -> {
+                copy()
+                true
+            }
+            R.id.item_history_options_clear -> {
+                clear()
+                true
+            }
+            else -> false
+        }
     }
 }
